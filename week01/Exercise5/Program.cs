13250c98e4d4efd7ad777860b97bd984b983c7c9
Testup476 <Kandajeanmarc@gmail.com>
2025-03-08 12:54:59 +0100

    modification

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
+    public static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    public static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name : ");
+        string username = Console.ReadLine();
+        return username;
+    }
+
+    public static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number :");
+        int usernumber = int.Parse(Console.ReadLine());
+        return usernumber;
+    }
+    public static int SquarteNumber(int number)
+    {
+        return number *= number;
+    }
+
+    public static void DisplayResult(string username, int SquarteNumber)
+    {
+        Console.WriteLine($"{username}, the square of your number is {SquarteNumber}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int Squartenumber = SquarteNumber(number);
+        DisplayResult(name, Squartenumber);
+
+
     }
 }
